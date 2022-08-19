4f0260cbd325448911716a10689e63df2453fe5a
Auston Pramodh Barboza <auston.barboza@cometchat.com>
2022-08-19 18:00:03 -0400

    chore: logging loki added

--- packages/api/src/logger/logger.module.ts
+++ packages/api/src/logger/logger.module.ts
@@ -2,6 +2,8 @@ import { Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 import { PrettyOptions } from 'pino-pretty';
+import * as pino from 'pino';
+import * as pinoLoki from 'pino-loki';
 
 import { Environment, LogLevel } from '../config/config.schema';
 
@@ -13,25 +15,38 @@ import { Environment, LogLevel } from '../config/config.schema';
       useFactory: async (configService: ConfigService) => {
         const environment = configService.get<Environment>('NODE_ENV');
         const loglevel = configService.get<LogLevel>('LOG_LEVEL');
+        const lokiHostname = configService.get<string>('LOKI_HOST');
+
+        const targets: pino.TransportTargetOptions[] = [];
+
+        if (environment === Environment.Development)
+          targets.push({
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              levelFirst: true,
+              translateTime: true,
+              // messageFormat: '{level} - {pid} - url:{req.url}',
+            } as PrettyOptions,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            level: loglevel!,
+          });
+
+        if (lokiHostname)
+          targets.push({
+            target: 'pino-loki',
+            options: {
+              host: lokiHostname,
+            } as Parameters<typeof pinoLoki.default>[0],
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            level: loglevel!,
+          });
 
         return {
           pinoHttp: {
             level: loglevel,
-            transport:
-              environment === Environment.Development
-                ? {
-                    target: 'pino-pretty',
-                    options: {
-                      colorize: true,
-                      levelFirst: true,
-                      translateTime: true,
-                      // messageFormat: '{level} - {pid} - url:{req.url}',
-                    } as PrettyOptions,
-                  }
-                : undefined,
-
-            formatters: {
-              level: (label): Record<string, unknown> => ({ level: label }),
+            transport: {
+              targets: targets,
             },
             autoLogging: environment !== Environment.Development,
           },
